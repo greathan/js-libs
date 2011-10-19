9334aa7e10aa3f81284f91e97040daf649bba74d
greathan <hwlyglove@gmail.com>
2011-10-19 17:31:38 +0800

    Update string/string.js

--- string/string.js
+++ string/string.js
@@ -4,3 +4,28 @@ String.prototype.toRegExpString = function(){
 String.prototype.trim = function(){
 	return this.replace(/^\s+|\s+$/g, '');
 };
+String.prototype.$replace = function(obj) {
+	return this.replace(/\$\{([\w\.?]+)\}/g, function(s, k) {
+		var keys = k.split('.'), l = keys.length;
+		var key = keys[0];
+		if (l > 1) {
+			var o = obj;
+			for (var i = 0; i < l; i++) {
+				if (key in o) {
+					o = o[key];
+					key = keys[i + 1];
+				} else return s;
+			}
+			return o;
+		}
+		return key in obj ? obj[key] : s;
+	});
+};
+String.prototype.format = function() {
+	var args = arguments, l = args.length;
+	if (l > 1) {
+		return this.replace(/\$(\d)/g, function(s, k) {
+			return args[k] == undefined ? '' : args[k];
+		});
+	} else return this;
+};

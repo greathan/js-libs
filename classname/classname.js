555974d770cd291103421edef5c74a743d636ed4
greathan <hwlyglove@gmail.com>
2011-12-06 14:28:00 +0800

    Update classname/classname.js

--- classname/classname.js
+++ classname/classname.js
@@ -1,14 +1,20 @@
 var ClassName = {
 	has: function(el, cls){
-		return new RegExp("\\b" + cls + "\\b").test(el.className);
+		return el.className.split('\s+').indexOf(cls) >= 0;
 	},
 	add: function(el, cls){
-		if (!this.has(el, cls)) el.className += ' ' + cls;
+		if (!this.has(el, cls)) {
+			var arr = el.className.split('\s+');
+			arr.push(cls);
+			el.className = arr.join(' ');
+		}
 	},
 	remove: function(el, cls){
-		var reg = new RegExp("\\b" + cls + "\\b");
-		if (this.has(el, cls))
-			el.className = el.className.replace(reg, '');
+		if (this.has(el, cls)) {
+			var arr = el.className.split('\s+');
+			arr.splice(arr.indexOf(cls), 1);
+			el.className = arr.join(' ');
+		}
 	},
 	toggle: function(el, cls){
 		el.className =
